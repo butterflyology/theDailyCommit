ea3ec54c4f6d48e08065f76ff9afb0fb05302f67
Chris Hamm <topher.hamm@gmail.com>
2023-05-30 11:22:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## The Daily Commit
+
+### Chris Hamm
+### 2023-05-30
+
+Well, look at you actually reading the contents of the repo. This repo exists to hold a `bash`script that will make a push every time you run it so it looks like you are super productive with GitHub. But in reality, you are just being awesome. And you, you are being more awesome by really reading the repo. Good on ya. 
---
+++ theDailyCommit.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+REPO_PATH="/Users/Chris/Projects/theDailyCommit"  # Specify the path to your repository
+COMMIT_MESSAGE="Daily commit"         # Specify the commit message
+
+cd "$REPO_PATH" || exit 1
+
+# Create a dummy file with the current date as the file name
+DATE=$(date +%Y-%m-%d)
+FILENAME="$DATE.txt"
+touch "$FILENAME"
+
+# Stage the new file
+git add "$FILENAME"
+
+# Commit the changes
+git commit -m "$COMMIT_MESSAGE"
+
+# Push the changes to the remote repository
+git push origin main
